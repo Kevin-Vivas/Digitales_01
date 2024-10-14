b18751f807864e1b9b494fba4daeac54db222819
Kevin Vivas <kevinalexvivas@gmail.com>
2024-10-13 21:30:57 -0500

    Se solucionó el problema de validación de contraseñas en la carpeta comunicacion_serial

--- 3.Comunicación_Serial/Comunicación_Serial.cpp
+++ 3.Comunicación_Serial/Comunicación_Serial.cpp
@@ -1,20 +1,18 @@
 #include "mbed.h"
 
-UnbufferedSerial PUERTO_PC(PA_9, PA_10, 9600); // (TX, RX)
-UnbufferedSerial PUERTO_CEL(PB_10, PB_11, 9600); // (TX, RX)
+UnbufferedSerial PUERTO_PC(PA_9, PA_10); // (TX, RX)
+UnbufferedSerial PUERTO_CEL(PB_10, PB_11); // (TX, RX)
 
-// Buffers para almacenar la entrada del usuario
 char RX_PC[50], RX_CEL[50];
 int index_PC = 0, index_CEL = 0;
 
 // Almacenar las contraseñas de las habitaciones
 char contrasena_habitacion[100][5] = {""};
 
-// Variables para el proceso del cliente
-int habitacion_cliente = -1;
+int habitacion_cliente = -1; // Variables para el proceso del cliente
 int etapa_cliente = 0;  // Variable para seguir el flujo del cliente
 
-// Prototipos de funciones
+// Funciones
 void limpiarBuffer(char* buffer, int& index);
 void procesarComandoPC();
 void procesarComandoCel();
@@ -22,20 +20,12 @@ void leerContrasena(int habitacion);
 void validarContrasenaCliente(int habitacion, const char* contrasena_cliente);
 void manejarPuertoPC();
 void manejarPuertoCel();
-void iniciarNuevoProceso(); // Nueva función para reiniciar el flujo
+void limpiarEntrada(char* entrada, int longitud); // Función para limpiar la entrada
 
 int main() {
-    while (1) {
-        iniciarNuevoProceso(); // Inicia todo el proceso de nuevo
-    }
-}
+    PUERTO_PC.write("\n\r\t\tBIENVENIDO AL HOTEL MAS CHIMBA!\n\r", 43);  
+    PUERTO_PC.write("\n\rSeleccione numero de la habitacion (3 caracteres):\n\r", 55);  
 
-// Nueva función para reiniciar todo el proceso
-void iniciarNuevoProceso() {
-    PUERTO_PC.write("\n\r\t\tBIENVENIDO AL HOTEL MAS CHIMBA!\n\r", 41);
-    PUERTO_PC.write("\n\rSeleccione numero de la habitacion (3 caracteres):\n\r", 51);
-
-    // Bucle principal
     while (1) {
         manejarPuertoPC();
         manejarPuertoCel();
@@ -77,11 +67,15 @@ void limpiarBuffer(char* buffer, int& index) {
 
 // Función para procesar los comandos recibidos por PC (Administrador)
 void procesarComandoPC() {
-    RX_PC[index_PC] = '\0';
-    habitacion_cliente = atoi(RX_PC);
+    RX_PC[index_PC] = '\0';    
+    char habitacion[4]; 
+    strncpy(habitacion, RX_PC, 3);
+    habitacion[3] = '\0'; 
+    
+    habitacion_cliente = atoi(habitacion);
+    
     char CADENA[100];
-
-    sprintf(CADENA, "\n\rAsigne la contrasena para habitacion %d (4 caracteres):\n\r", habitacion_cliente);
+    sprintf(CADENA, "\n\rAsigne la contrasena para habitacion %s (4 caracteres):\n\r", habitacion);
     PUERTO_PC.write(CADENA, strlen(CADENA));
 
     leerContrasena(habitacion_cliente);
@@ -90,23 +84,34 @@ void procesarComandoPC() {
 
 // Función para procesar los comandos recibidos por el celular (Cliente)
 void procesarComandoCel() {
-    RX_CEL[index_CEL] = '\n'; // Asegura que el buffer sea un string
+    RX_CEL[index_CEL] = '\0';
     char CADENA[100];
 
-    if (etapa_cliente == 1) {  // El cliente ingresa la habitacion
-        habitacion_cliente = atoi(RX_CEL);
-        sprintf(CADENA, "\n\rHas ingresado a la habitacion %d.\n\r", habitacion_cliente);
+    if (etapa_cliente == 1) {  
+        sprintf(CADENA, "\n\rIngrese la contraseña para la habitacion %d (4 caracteres):\n\r", habitacion_cliente);
         PUERTO_CEL.write(CADENA, strlen(CADENA));
-
-        sprintf(CADENA, "\n\rIngrese la contraseña para la habitacion %d:\n\r", habitacion_cliente);
-        PUERTO_CEL.write(CADENA, strlen(CADENA));
-        etapa_cliente = 2;  // Cambia a la etapa de ingresar la contraseña
-    } else if (etapa_cliente == 2) {  // El cliente ingresa la contraseña
-        validarContrasenaCliente(habitacion_cliente, RX_CEL);
-        // No reinicia el flujo aquí para volver a pedir contraseña si es incorrecta
+        etapa_cliente = 2;  
+        limpiarBuffer(RX_CEL, index_CEL);
+    } else if (etapa_cliente == 2) {  
+        char contrasena_cliente[50]; 
+        strncpy(contrasena_cliente, RX_CEL, 50);
+        limpiarEntrada(contrasena_cliente, 50);
+        validarContrasenaCliente(habitacion_cliente, contrasena_cliente);
+        limpiarBuffer(RX_CEL, index_CEL);
     }
+}
 
-    limpiarBuffer(RX_CEL, index_CEL);
+// Función para limpiar caracteres no deseados de la entrada
+void limpiarEntrada(char* entrada, int longitud) {
+    char temp[50] = {0}; 
+    int j = 0;
+
+    for (int i = 0; i < longitud; i++) {
+        if (entrada[i] != '\n' && entrada[i] != '\r' && entrada[i] != ' ') {
+            temp[j++] = entrada[i];
+        }
+    }
+    strncpy(entrada, temp, longitud);
 }
 
 // Función para leer y asignar la contraseña (Administrador)
@@ -120,46 +125,45 @@ void leerContrasena(int habitacion) {
             index_temp++;
         }
     }
-
+    
     temp[4] = '\0';
     strcpy(contrasena_habitacion[habitacion], temp);
 
     char CADENA[100];
-    sprintf(CADENA, "\n\rContrasena asignada con exito!\n\r");
+    sprintf(CADENA, "\n\rLa contraseña es %s\n\r", temp);
     PUERTO_PC.write(CADENA, strlen(CADENA));
+    PUERTO_PC.write("\n\rContrasena asignada con exito!\n\r", 34);  
 
-    // Enviar mensaje al cliente pidiéndole ingresar la habitación
     sprintf(CADENA, "\n\rBienvenido! Por favor, ingrese el numero de la habitacion asignada:\n\r");
     PUERTO_CEL.write(CADENA, strlen(CADENA));
-    etapa_cliente = 1;  // Cambia la etapa del cliente para que ingrese la habitación
+    etapa_cliente = 1;  
 }
 
 // Función para validar la contraseña del cliente
 void validarContrasenaCliente(int habitacion, const char* contrasena_cliente) {
-   // Comprobar si la longitud de la contraseña ingresada es 4
-    if (strlen(contrasena_cliente) != 4) {
-        char CADENA[100];
-        sprintf(CADENA, "\n\rLa contraseña debe tener 4 caracteres. Intente nuevamente:\n\r");
-        PUERTO_CEL.write(CADENA, strlen(CADENA));
-        return; // Salir de la función sin reiniciar el flujo
-    }
-
-    // Validar la contraseña
+    char CADENA[100];
     if (strcmp(contrasena_habitacion[habitacion], contrasena_cliente) == 0) {
-        char CADENA[100];
-        sprintf(CADENA, "Acceso a habitacion %d autorizado.", habitacion);
+        sprintf(CADENA, "\n\rAcceso a habitacion %d autorizado.\n\r", habitacion);
         PUERTO_CEL.write(CADENA, strlen(CADENA));
         sprintf(CADENA, "\n\rEl usuario ingreso de forma correcta a la habitacion %d.\n\r", habitacion);
         PUERTO_PC.write(CADENA, strlen(CADENA));
+        
+        // Reiniciar flujo
+        habitacion_cliente = -1;
+        etapa_cliente = 0;
+        index_PC = 0;
+        index_CEL = 0;
+        memset(RX_PC, 0, sizeof(RX_PC));
+        memset(RX_CEL, 0, sizeof(RX_CEL));
+        PUERTO_PC.write("\n\rSeleccione numero de la habitacion (3 caracteres):\n\r", 55);
     } else {
-        char CADENA[100];
         sprintf(CADENA, "\n\rAcceso a habitacion %d denegado. Contrasena incorrecta.\n\r", habitacion);
         PUERTO_CEL.write(CADENA, strlen(CADENA));
-        sprintf(CADENA, "\n\rIntento de ingreso a la habitacion %d sin contraseña correcta.\n\r", habitacion);
+        sprintf(CADENA, "\n\rIntento de ingreso a la habitacion %d con contraseña incorrecta.\n\r", habitacion);
         PUERTO_PC.write(CADENA, strlen(CADENA));
 
-        // Volver a pedir la contraseña al cliente
-        sprintf(CADENA, "\n\rIngrese nuevamente la contraseña para la habitacion %d:\n\r", habitacion);
+        sprintf(CADENA, "\n\rPor favor, intente ingresar la contraseña nuevamente:\n\r");
         PUERTO_CEL.write(CADENA, strlen(CADENA));
+        etapa_cliente = 2;
     }
 }
